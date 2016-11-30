a40b3973f0b3b3c2226a30e5bd266960768798e8
Kiyotaka Saitou <kiyotaka-saitou@united.jp>
2016-11-30 09:40:16 +0900

    GoForwardUnityChanプロジェクト 最初のコミット
    基本ゲーム機能の実装と効果音の追加を行いました。

---
+++ Assets/GoForwardUnityChan/Script/CubeController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -10;
+
+	// 効果音
+	private AudioSource soundEffect;
+
+	// Use this for initialization
+	void Start(){
+		soundEffect = GetComponent<AudioSource>();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		// キューブを移動させる
+		transform.Translate (this.speed, 0, 0);
+
+		// 画面外に出たら破棄する
+		if (transform.position.x < this.deadLine){
+			Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.transform.tag == "GroundTag" || other.transform.tag == "CubeTag")
+			soundEffect.PlayOneShot (soundEffect.clip);
+	}
+
+}
